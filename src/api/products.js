d5b948f057134489b2baef031ca75c115be234bb
belopot <belopot@protonmail.com>
2020-12-09 11:35:03 -0400

    Deploy app to roast

--- src/api/products.js
+++ src/api/products.js
@@ -1,13 +1,13 @@
 const ProductData = [
   {
     title: "1918<br/>Establishment",
-    texturePath: "/images/1.png",
+    texturePath: "/assets/images/1.png",
     position: { x: -80, y: 0 },
     annoDir: { x: 0, y: 1.1 },
     color: 0x025de4,
     hotspot: {
       template: "template1",
-      image: "/images/1.png",
+      image: "/assets/images/1.png",
       title: "The Begining - Konosuke Matsushita",
       desc:
         "Konosuke Matsushita began his mission to empower the world more than 100 years ago.",
@@ -15,13 +15,13 @@ const ProductData = [
   },
   {
     title: "1931<br/>The First Dry<br/>Battery",
-    texturePath: "/images/2.png",
+    texturePath: "/assets/images/2.png",
     position: { x: -61.484, y: -27.27 },
     annoDir: { x: -1.45, y: 0 },
     color: 0x0261e5,
     hotspot: {
       template: "template1",
-      image: "/images/2.png",
+      image: "/assets/images/2.png",
       title: "The First Dry Battery",
       desc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
@@ -29,13 +29,13 @@ const ProductData = [
   },
   {
     title: "1994<br/>Introduces Rechargeable",
-    texturePath: "/images/3.png",
+    texturePath: "/assets/images/3.png",
     position: { x: -29.671, y: -36.62 },
     annoDir: { x: 0, y: -1.1 },
     color: 0x01b3f7,
     hotspot: {
       template: "template1",
-      image: "/images/3.png",
+      image: "/assets/images/3.png",
       title: "Introduces Rechargeable",
       desc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
@@ -43,13 +43,13 @@ const ProductData = [
   },
   {
     title: "1997<br/>Automotive<br/>Ni-Hd",
-    texturePath: "/images/4.png",
+    texturePath: "/assets/images/4.png",
     position: { x: -3.231, y: -16.425 },
     annoDir: { x: -1.1, y: 0.6 },
     color: 0x01d6f7,
     hotspot: {
       template: "template1",
-      image: "/images/4.png",
+      image: "/assets/images/4.png",
       title: "Automotive Ni-Hd",
       desc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
@@ -57,13 +57,13 @@ const ProductData = [
   },
   {
     title: "2010<br/>Lib Auto<br/>Cylindrical",
-    texturePath: "/images/5.png",
+    texturePath: "/assets/images/5.png",
     position: { x: 3.231, y: 16.425 },
     annoDir: { x: 1.1, y: -0.6 },
     color: 0x01eef5,
     hotspot: {
       template: "template1",
-      image: "/images/5.png",
+      image: "/assets/images/5.png",
       title: "Lib Auto Cylindrical",
       desc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
@@ -71,13 +71,13 @@ const ProductData = [
   },
   {
     title: "2017<br/>Lib For Multi Application",
-    texturePath: "/images/6.png",
+    texturePath: "/assets/images/6.png",
     position: { x: 29.671, y: 36.62 },
     annoDir: { x: 0, y: 1.1 },
     color: 0x01fca6,
     hotspot: {
       template: "template1",
-      image: "/images/6.png",
+      image: "/assets/images/6.png",
       title: "Lib For Multi Application",
       desc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
@@ -85,13 +85,13 @@ const ProductData = [
   },
   {
     title: "Now<br/>Environmental<br/>Priority",
-    texturePath: "/images/7.png",
+    texturePath: "/assets/images/7.png",
     position: { x: 61.484, y: 27.27 },
     annoDir: { x: 1.5, y: 0 },
     color: 0x01fca6,
     hotspot: {
       template: "template1",
-      image: "/images/7.png",
+      image: "/assets/images/7.png",
       title: "Environmental Priority",
       desc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
@@ -99,13 +99,13 @@ const ProductData = [
   },
   {
     title: "The Future<br/>Toward Carbon<br/>Neutrality",
-    texturePath: "/images/8.png",
+    texturePath: "/assets/images/8.png",
     position: { x: 80.0, y: 0 },
     annoDir: { x: 0, y: -1.2 },
     color: 0x01fe6d,
     hotspot: {
       template: "template1",
-      image: "/images/8.png",
+      image: "/assets/images/8.png",
       title: "Toward Carbon Neutrality",
       desc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
